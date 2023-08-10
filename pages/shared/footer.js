da42e3dd0e18d0ca48de23a406cb212e0ce50c96
Seto Elkahfi <setoelkahfi@gmail.com>
2023-08-10 11:18:36 +0200

    Add react icons

--- pages/shared/footer.js
+++ pages/shared/footer.js
@@ -1,12 +1,13 @@
 import styles from '@/styles/Shared.module.css'
+import { BsGithub } from 'react-icons/bs'
 
 export default function SharedFooter(props) {
     return (
         <>
             <div className={styles.footer}>
-                <div>
-                    {props.breadcrumb}
-                </div>
+                <a href={process.env.NEXT_PUBLIC_GITHUB_URL} rel="noopener noreferrer" target='__blank'>
+                    <BsGithub size={25}/>
+                </a>
             </div>
         </>
     )
