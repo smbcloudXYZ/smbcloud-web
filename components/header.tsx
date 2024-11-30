6df106993cb24648718d6d60325fe9c507aa99d5
Seto Elkahfi <hej@setoelkahfi.se>
2024-11-30 17:06:11 +0100

    link

--- components/header.tsx
+++ components/header.tsx
@@ -9,6 +9,7 @@ export default function Header() {
         <nav className="ml-auto text-sm font-medium space-x-6">
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
+          <Link href="https://console.smbcloud.xyz" target="__blank">Console</Link>
         </nav>
       </div>
     </header>
