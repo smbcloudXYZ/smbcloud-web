077d2f48ad7342605708455cef19f71da3f4e379
Seto Elkahfi <setoelkahfi@gmail.com>
2025-06-22 14:53:01 +0200

    Update homepage text to mention Astro and NodeJS apps

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 import { allPosts } from "@/.contentlayer/generated";
 import Link from "next/link";
 import { genPageMetadata } from "./seo";
-import siteMetadata from '@/content/siteMetadata'
+import siteMetadata from "@/content/siteMetadata";
 import TabbedRunner from "./components/TabbedRunner";
 
-export const metadata = genPageMetadata({ title: `${siteMetadata.title}` })
+export const metadata = genPageMetadata({ title: `${siteMetadata.title}` });
 
 export default function Home() {
-  let publishedPosts = allPosts.filter((post) => post.published)
-  publishedPosts = publishedPosts.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+  let publishedPosts = allPosts.filter((post) => post.published);
+  publishedPosts = publishedPosts.sort(
+    (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
+  );
   return (
     <div className="prose dark:prose-invert">
       <h1>smbCloud</h1>
-      <p className="text-lg">Deploy your NextJS app with one command: </p>
+      <p className="text-lg">
+        Deploy your NextJS, Astro, or any NodeJS app with one command:{" "}
+      </p>
       <TabbedRunner />
       <hr />
       <h2>Resources</h2>
